e340bf6713d3144b0ed115e28defbe1b34b43b56
Valentin T <Eywek@users.noreply.github.com>
2018-02-20 17:27:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const cli = require('./lib/cli')
 if (process.argv.length > 2)
-    cli()
+    return cli()
 
 console.info('Launching daemons...')
 require('./lib/daemon')(require('./config/config.json').daemons)
